13fc1b6c868e9021918660779cc7073578dd0501
Luna <1017929760@qq.com>
2020-05-14 18:21:37 +0800

    使用ref

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
 class App extends React.PureComponent {
+    divRef = undefined
     constructor(props) {
         super(props);
         this.state = {
             n: 1,
             width:undefined
-        }
+        };
+        this.divRef = React.createRef()
     }
 
     onClick = () => {
         this.setState(state => ({n: state.n + 1}))
     }
     componentDidMount() {
-        const div = document.getElementById('xxx')
-        const {width} = div.getBoundingClientRect()
+        const div=this.divRef.current
+        console.log(div);
+        const {width} =div.getBoundingClientRect()
         this.setState({width})
     }
 
     render() {
         return(
-            <div id="xxx">Hello World,{this.state.width}px</div>
+            <div ref={this.divRef}>Hello World,{this.state.width}px</div>
         )
     }
 }
