81bd89886990d99757c6a26b0a326eece7e8e3ff
Luna <1017929760@qq.com>
2020-05-14 20:52:50 +0800

    用useEffect模拟componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React, {useEffect, useState} from 'react';
 
 const App =props=>{
     const [n,setN] = useState(0)
-    const onClick = ()=>{
+    const [m,setM] = useState(0)
+    const onClickN = ()=>{
         setN(n+1)
-    }
+    };
+    const onClickM = ()=>{
+        setM(m+1)
+    };
+
     useEffect(()=>{
         console.log('第一次渲染')
     },[])
+
+    useEffect(()=>{
+        console.log('n变了')
+    },[n])
+
     return(
         <div>
             {n}
-            <button onClick={onClick}>+1</button>
+            <button onClick={onClickN}>n+1</button>
+            <hr />
+            {m}
+            <button onClick={onClickM}>m+1</button>
         </div>
     )
 }
