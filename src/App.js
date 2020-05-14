72bc509a4fff23326742bf3b92351fe3dbb5afd0
Luna <1017929760@qq.com>
2020-05-14 17:23:49 +0800

    å在render中使用ifã...else

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import React from 'react';
 class App extends React.PureComponent {
     constructor(props) {
         super(props);
-        this.state={
-            n:1
+        this.state = {
+            n: 1
         }
     }
-    onClick=()=>{
-        this.setState(state=>({n:state.n+1}))
-        this.setState(state=>({n:state.n-1}))
+
+    onClick = () => {
+        this.setState(state => ({n: state.n + 1}))
     }
-    render(){
-        console.log('render 了一次')
+
+    render() {
+        let message
+        if (this.state.n % 2 === 0) {
+            message=<div>偶数</div>
+        } else {
+            message=<span>奇数</span>
+        }
         return(
-            <div>
-                App
-                <div>
-                    {this.state.n}
-                    <button onClick={this.onClick}>+1</button>
-                </div>
-            </div>
+            <>
+                {message}
+                <button onClick={this.onClick}>+1</button>
+            </>
         )
     }
 }
