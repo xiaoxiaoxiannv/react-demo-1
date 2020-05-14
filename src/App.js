4f39d199b9583616a4ba59f212015e16844a9c1b
Luna <1017929760@qq.com>
2020-05-14 16:14:59 +0800

    练习使用componentWillReceiceProps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={x:1}
+    }
+    onClick=()=>{
+        this.setState({x:this.state.x+1},
+            ()=>{
+                this.setState({x:this.state.x+1})
+            })
+    }
+    onClick2=()=>{
+        this.setState((state)=>({x:state.x+1}))
+        this.setState((state)=>({x:state.x+1}))
+    }
+    render() {
+        return (
+            <div className="App">
+                App<button onClick={this.onClick2}>+2</button>
+                <B name={this.state.x}/>
+            </div>
+        )
+    }
+}
+
+class B extends React.Component {
+    componentWillReceiveProps(newProps, nextContext) {
+        console.log('旧的 props')
+        console.log(this.props)
+        console.log('props变化了')
+        console.log('新的 props')
+        console.log(newProps)
+    }
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
 }
 
 export default App;
