1e5beee1ecf6515455b5b90154bf82534bde8e28
Luna <1017929760@qq.com>
2020-05-14 17:00:43 +0800

    练习shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -3,44 +3,32 @@ import React from 'react';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state={x:1}
+        this.state={
+            n:1
+        }
     }
     onClick=()=>{
-        this.setState({x:this.state.x+1},
-            ()=>{
-                this.setState({x:this.state.x+1})
-            })
+        this.setState(state=>({n:state.n+1}))
+        this.setState(state=>({n:state.n-1}))
     }
-    onClick2=()=>{
-        this.setState((state)=>({x:state.x+1}))
-        this.setState((state)=>({x:state.x+1}))
+    shouldComponentUpdate(newProps, newState) {
+        if(newState.n === this.state.n){
+            return false
+        }else{
+            return true
+        }
     }
-    render() {
-        return (
-            <div className="App">
-                App<button onClick={this.onClick2}>+2</button>
-                <B name={this.state.x}/>
+
+    render(){
+        console.log('render 了一次')
+        return(
+            <div>
+                App
+                <div>
+                    {this.state.n}
+                    <button onClick={this.onClick}>+1</button>
+                </div>
             </div>
-        )
-    }
-}
-
-class B extends React.Component {
-    componentWillReceiveProps(newProps, nextContext) {
-        console.log('旧的 props')
-        console.log(this.props)
-        console.log('props变化了')
-        console.log('新的 props')
-        console.log(newProps)
-    }
-
-    constructor() {
-        super();
-    }
-
-    render() {
-        return (
-            <div>{this.props.name}</div>
         )
     }
 }
