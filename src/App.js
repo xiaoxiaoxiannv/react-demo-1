30cfb696d821503ee8b589e7b115a048ca69f730
Luna <1017929760@qq.com>
2020-05-15 16:14:22 +0800

    理解useState原理

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React, {useState} from 'react';
-import useUpdate from "./useUpdate.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+const rootElement = document.getElementById('root')
+//import useUpdate from "./useUpdate.js";
+
+let _state = [];
+let index =0;
+const myUseState = (initialValue)=>{
+    const currentIndex = index
+    _state[currentIndex]=_state[currentIndex] === undefined ? initialValue : _state[currentIndex];
+    const setState = newValue =>{
+        _state[currentIndex] = newValue;
+        render()
+    }
+    index += 1
+    return [_state[currentIndex],setState]
+}
+
+const render = ()=>{
+    index = 0
+    ReactDOM.render(<App/>,rootElement)
+}
 
 const App = props => {
-    const [n, setN] = useState(0)
-    const onClick = () => {
+    const [n, setN] = myUseState(0)
+    const [m, setM] = myUseState(0)
+    const onClickN = () => {
         setN(n + 1)
     }
-
-    useUpdate(() => {
-        console.log("变了")
-    }, n)
+    const onClickM = () => {
+        setM(m + 1)
+    }
 
     return (
         <div>
             {n}
-            <button onClick={onClick}>+1</button>
+            <button onClick={onClickN}>n+1</button>
+            <hr />
+            {m}
+            <button onClick={onClickM}>m+1</button>
         </div>
     )
 }
