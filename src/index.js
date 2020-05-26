9ca8d73c1f87b4069ce9122978b4b436f0eef105
Luna <1017929760@qq.com>
2020-05-26 18:16:08 +0800

    用useReducer代替Reduxâ

--- src/index.js
+++ src/index.js
@@ -1,36 +1,124 @@
-import React, {useReducer, useState} from 'react';
+import React, {createContext, useContext, useEffect, useReducer} from 'react';
 import ReactDOM from 'react-dom';
 
-const initial = {
-    n:0
-};
-const reducer = (state,action)=>{
-    if(action.type === 'add'){
-        return {n:state.n+action.number}
-    }else if(action.type === 'mult'){
-        return {n:state.n*2}
-    }else{
-        throw new Error('unknown type')
+const store = {
+    user: null,
+    books: null,
+    movies: null
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case'setUser':
+            return {...state, user: action.user}
+        case 'setBooks':
+            return {...state, books: action.books}
+        case 'setMovies':
+            return {...state, movies: action.movies}
+        default:
+            throw new Error()
     }
-};
+}
+
+const Context = createContext(null)
 
 function App() {
-    const [state,dispatch] =useReducer(reducer,initial);
-    const onClick = ()=>{
-        dispatch({type:'add',number:1})
-    };
-    const onClick2 = ()=>{
-        dispatch({type:'add',number:2})
-    };
-    return(
-        <div className="App">
-            <h1>n:{state.n}</h1>
-            <button onClick={onClick}>+1</button>
-            <button onClick={onClick2}>+2</button>
+    const [state, dispatch] = useReducer(reducer, store)
+    return (
+        <Context.Provider value={{state, dispatch}}>
+            <User/>
+            <hr/>
+            <Books/>
+            <Movies/>
+        </Context.Provider>
+    )
+}
+
+function User() {
+    const {state, dispatch} = useContext(Context)
+    useEffect(() => {
+        ajax("/user").then(user => {
+            dispatch({type: "setUser", user})
+        })
+    }, [])
+    return (
+        <div>
+            <h1>个人信息</h1>
+            <div>name:{state.user ? state.user.name : ""}</div>
+        </div>
+    )
+}
+
+function Books() {
+    const {state, dispatch} = useContext(Context);
+    useEffect(() => {
+        ajax("/books").then(books => {
+            dispatch({type: "setBooks", books: books});
+        });
+    }, []);
+    return (
+        <div>
+            <h1>我的书籍</h1>
+            <ol>
+                {state.books ? state.books.map(book => <li key={book.id}>{book.name}</li>) : "加载中"}
+            </ol>
+        </div>
+    );
+}
+
+function Movies() {
+    const {state, dispatch} = useContext(Context);
+    useEffect(() => {
+        ajax("/movies").then(movies => {
+            dispatch({type: "setMovies", movies: movies});
+        });
+    }, []);
+    return (
+        <div>
+            <h1>我的电影</h1>
+            <ol>
+                {state.movies
+                    ? state.movies.map(movie => <li key={movie.id}>{movie.name}</li>)
+                    : "加载中"}
+            </ol>
         </div>
     )
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />,rootElement);
+ReactDOM.render(<App/>, rootElement);
 
+function ajax(path) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (path === "/user") {
+                resolve({
+                    id: 1,
+                    name: "Frank"
+                });
+            } else if (path === "/books") {
+                resolve([
+                    {
+                        id: 1,
+                        name: "JavaScript 高级程序设计"
+                    },
+                    {
+                        id: 2,
+                        name: "JavaScript 精粹"
+                    }
+                ]);
+            } else if (path === "/movies") {
+                resolve([
+                    {
+                        id: 1,
+                        name: "爱在黎明破晓前"
+                    },
+                    {
+                        id: 2,
+                        name: "恋恋笔记本"
+                    }
+                ]);
+            }
+        }, 2000);
+    });
+}
