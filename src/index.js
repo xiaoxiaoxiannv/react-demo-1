cb5b19170ef2fb4b679df5b2a2cf67d5753239b8
Luna <1017929760@qq.com>
2020-05-26 15:43:44 +0800

    useReducer的使用

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React ,{useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const initial = {
+    n:0
+};
+const reducer = (state,action)=>{
+    if(action.type === 'add'){
+        return {n:state.n+action.number}
+    }else if(action.type === 'mult'){
+        return {n:state.n*2}
+    }else{
+        throw new Error('unknown type')
+    }
+};
+
 function App() {
-    const [n,setN] = useState(0);
+    const [state,dispatch] =useReducer(reducer,initial);
     const onClick = ()=>{
-        setN(i => i+1);
-        setN(i => i+1);
+        dispatch({type:'add',number:1})
+    };
+    const onClick2 = ()=>{
+        dispatch({type:'add',number:2})
     };
     return(
         <div className="App">
-            <h1>n:{n}</h1>
-            <button onClick={onClick}>+2</button>
+            <h1>n:{state.n}</h1>
+            <button onClick={onClick}>+1</button>
+            <button onClick={onClick2}>+2</button>
         </div>
     )
 }
