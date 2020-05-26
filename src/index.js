aaff94dfe761f686e33bad430f7776fff07ff8a9
Luna <1017929760@qq.com>
2020-05-26 21:55:33 +0800

    使用useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useLayoutEffect } from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-    const [n,setN] =useState(0)
-    const onClick = ()=>{
-        setN(i=>i+1)
-    }
-   useEffect(()=>{
-       console.log('第一次渲染之后执行这一句话')
-   },[])
-    useEffect(()=>{
-        console.log('n变化了')
-    },[n])
+const App = () => {
+    const [value, setValue] = useState(0);
+    useLayoutEffect(() => {
+        document.querySelector('#x').innerText = `value: 1000`
+    }, [value]);
+
     return (
-            <div>
-                n:{n}
-                <button onClick={onClick}>+1</button>
-            </div>
+        <div id="x" onClick={() => setValue(0)}>value: {value}</div>
     );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
