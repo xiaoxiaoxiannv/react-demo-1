e7afb62aaf9f6423d8497f2fa8f445b4193e1080
Luna <1017929760@qq.com>
2020-05-27 15:49:50 +0800

    使用forwardRef

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import React, {useState, useLayoutEffect, useRef, useEffect} from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 
+
 function App() {
-    console.log('App执行了')
-    const count = useRef(0)
-    const [n, setN] = React.useState(0);
-    const onClick = () => {
-        setN(n + 1);
-    };
-    useEffect(()=>{
-        count.current +=1;
-        console.log(count.current)
-    });
+    const buttonRef = useRef(null);
     return (
         <div className="App">
-            <div>
-                <button onClick={onClick}>update n {n}</button>
-            </div>
+            <Button2 ref={buttonRef}>按钮</Button2>
         </div>
     );
 }
 
+const Button2 = React.forwardRef((props,ref)=>{
+    return <button className="red" ref={ref} {...props} />
+});
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
